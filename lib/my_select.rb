5f29f44bac779b83652ef2673c943b72e8dc0ef2
davidtom <davidjtomczyk@gmail.com>
2017-05-22 09:57:31 -0400

    completed my_select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_collection = Array.new
+ while i < collection.length
+   evaluation = yield (collection[i])
+   if evaluation
+     new_collection.push(collection[i])
+   end
+   i += 1
+ end
+ new_collection
 end
